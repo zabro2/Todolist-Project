172c1e8712cadad1f97bb504bf417e22e7bd47fb
Zach G <zabros20@gmail.com>
2019-03-01 14:10:22 -0700

    Commit 3, added delete functionality to all x buttons. changed some styling and added color animations to all icons

--- action.js
+++ action.js
@@ -7,7 +7,7 @@ function addNewList() {
         $(".lists").append("<div class='list' id='list" + counter + "'>" +
             "<div class='header'>" +
             "<div class='listtitleholder'>" +
-            "<i class='fas fa-times timeslistname'></i>" +
+            "<i class='fas fa-times timeslistname' onclick='deleteList(this)'></i>" +
             "<div class='listtitle' contenteditable='true'>" + listName +
             "</div>" +
             "<i class='fas fa-plus' onclick='addNewItem(" + counter + ")'></i>" +
@@ -31,14 +31,34 @@ function checkKey(event) {
 function addNewItem(par) {
     let itemHolder = $("#itemholder" + par);
     itemHolder.append("<div class='item'>" +
-        "<i class='fas fa-times'>" +
+        "<i class='fas fa-times' onclick='deleteItem(this)'>" +
         "</i>" +
         "<div class='itemname' contenteditable='true'>sample text" +
         "</div>" +
-        "<button id='markdonebtn' onclick='markDone(this)'>done</button>" +
+        "<i class='fas fa-check' onclick='markDone(this)'></i>" +
         "</div>");
 }
 
+function deleteList(element) {
+    $(element).parent().parent().parent().remove();
+}
+
+function deleteItem(element) {
+    $(element).parent().remove();
+}
+
 function markDone(element) {
-    $(element).parent().addClass('done');
+    if ($(element).parent().hasClass('done')) {
+        $(element).parent().removeClass('done');
+        // $(element).prev().css('text-decoration', 'none');
+        $(element).css('color', 'black');
+    } else {
+        $(element).parent().addClass('done');
+        // $(element).prev().css('text-decoration', 'line-through');
+        $(element).css('color', 'orange');
+    }
+}
+
+function clearCompleted() {
+    $(".done").remove();
 }
