52b357e3b867770f1243d38a848300e3f3fdfa3c
Zach G <zabros20@gmail.com>
2019-03-01 11:11:02 -0700

    Commit 2, added finished button

--- action.js
+++ action.js
@@ -4,19 +4,19 @@ function addNewList() {
     let listName = $("#newlistname").val();
     if (listName != "") {
         /* put list html code here */
-        $(".lists").append("<div id='list" + counter +  "'>" +
-            "<div class='header'>" + 
-            "<div class='listtitleholder'>" + 
-            "<i class='fas fa-times'></i>" +
-            "<div class='listtitle' contenteditable='true'>" + listName + 
-            "</div>" + 
+        $(".lists").append("<div class='list' id='list" + counter + "'>" +
+            "<div class='header'>" +
+            "<div class='listtitleholder'>" +
+            "<i class='fas fa-times timeslistname'></i>" +
+            "<div class='listtitle' contenteditable='true'>" + listName +
+            "</div>" +
             "<i class='fas fa-plus' onclick='addNewItem(" + counter + ")'></i>" +
             "</div>" +
-            "<div class='itemholder'>" +
-            "</div>" + "</div>");
+            "</div>" + "<div class='itemholder' id='itemholder" + counter + "'>" +
+            "</div>");
         $("#newlistname").val("");
         $("#newlistname").focus();
-        counter ++;
+        counter++;
     }
 }
 
@@ -29,10 +29,16 @@ function checkKey(event) {
 }
 
 function addNewItem(par) {
-    let itemHolder = $("#list" + par);
+    let itemHolder = $("#itemholder" + par);
     itemHolder.append("<div class='item'>" +
         "<i class='fas fa-times'>" +
         "</i>" +
         "<div class='itemname' contenteditable='true'>sample text" +
-        "</div>" + "</div>");
+        "</div>" +
+        "<button id='markdonebtn' onclick='markDone(this)'>done</button>" +
+        "</div>");
+}
+
+function markDone(element) {
+    $(element).parent().addClass('done');
 }
